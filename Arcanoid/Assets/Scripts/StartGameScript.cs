8e7870505b4e0b91c59b4de0dc32e7a320f51c86
ujuzy <igorazumkov@gmail.com>
2020-05-24 15:20:14 +0300

    Fixed start and win screen

--- Arcanoid/Assets/Scripts/StartGameScript.cs
+++ Arcanoid/Assets/Scripts/StartGameScript.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartGameScript : MonoBehaviour
 {
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
